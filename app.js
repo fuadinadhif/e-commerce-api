36cc74dbf656c95c58fc7e6cb0ebc50c14d5a7ba
fuadinadhif <nadhiffuadi@gmail.com>
2022-06-16 17:28:59 +0700

    provide mock data link preview

--- app.js
+++ app.js
@@ -28,7 +28,13 @@ const userRouter = require("./routes/user-route");
 const productRouter = require("./routes/product-route");
 const reviewRouter = require("./routes/review-route");
 const orderRouter = require("./routes/order-route");
+// mock data
+const userData = require("./mock-data/users.json");
+const productData = require("./mock-data/products.json");
+const reviewData = require("./mock-data/reviews.json");
+const orderData = require("./mock-data/orders.json");
 
+// express package routes and fuctions
 app.use(xss());
 app.set("trust proxy", 1);
 app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 60 }));
@@ -39,6 +45,7 @@ app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.json());
 app.use(express.static("./public"));
 
+// documentation routes and functions
 const swaggerDocument = YAML.load("./e-commerce-api-docs.yaml");
 const options = {
   customSiteTitle: "e-Commerce API Docs",
@@ -46,18 +53,31 @@ const options = {
 app.use("/", swaggerUI.serve);
 app.get("/", swaggerUI.setup(swaggerDocument, options));
 
+// reset and mock data routes and functions
+const mockData = [
+  { users: [...userData] },
+  { products: [...productData] },
+  { reviews: [...reviewData] },
+  { orders: [...orderData] },
+];
 const factoryReset = require("./controllers/factory-reset-controller");
 app.post("/api/v1/factory-reset", factoryReset);
+app.get("/api/v1/mock-data", (req, res) => {
+  res.status(200).send(mockData);
+});
 
+// main routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/reviews", reviewRouter);
 app.use("/api/v1/orders", orderRouter);
 
+// fallback routes
 app.use(notFoundMDW);
 app.use(errorMWD);
 
+// connect server
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
